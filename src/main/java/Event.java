767992778e43593522d85ddb094c789c15f9a483
seanrh34 <34706751+seanrh34@users.noreply.github.com>
2025-08-21 21:21:39 +0800

    Fixed issue where Event tasks do not show to and from values

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,16 @@
 public class Event extends Task{
 
-    protected String by;
     protected String from;
+    protected String to;
 
-    public Event(String description, String by, String from) {
+    public Event(String description, String from, String to) {
         super(description);
-        this.by = by;
         this.from = from;
+        this.to = to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + by + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
