b0acf5921c149b53d73c700bac72156314ce6591
seanrh34 <34706751+seanrh34@users.noreply.github.com>
2025-08-21 15:16:32 +0800

    Echo

--- src/main/java/JohnChatBot.java
+++ src/main/java/JohnChatBot.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class JohnChatBot{
+    // Fixed Messages
+    private static final String LINE = "=================================================\n";
+    private static final String greetingMsg = LINE
+            + "Hello! I'm JohnChatBot.\n"
+            + "What can I do for you?\n"
+            + LINE;
+
+    private static final String exitMsg = LINE
+            + "Bye. Hope to see you again soon!\n"
+            + LINE;
+
     public static void main(String[] args) {
-        String msg = "=================================================\n"
-                + "Hello! I'm JohnChatBot.\n"
-                + "What can I do for you?\n"
-                + "=================================================\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "=================================================\n";
-        System.out.println(msg);
+        System.out.println(greetingMsg);
+
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+
+        while (sc.hasNextLine() && !exit) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(exitMsg);
+                exit = true;
+            } else {
+                System.out.println(LINE + input + '\n' + LINE);
+            }
+        }
     }
 }
