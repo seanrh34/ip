faa551d6166ac41b656f06760049f5def927778a
seanrh34 <34706751+seanrh34@users.noreply.github.com>
2025-08-21 14:55:00 +0800

    Rename, Greet, Exit

---
+++ src/main/java/JohnChatBot.java
@@ -0,0 +1,11 @@
+public class JohnChatBot{
+    public static void main(String[] args) {
+        String msg = "=================================================\n"
+                + "Hello! I'm JohnChatBot.\n"
+                + "What can I do for you?\n"
+                + "=================================================\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "=================================================\n";
+        System.out.println(msg);
+    }
+}
