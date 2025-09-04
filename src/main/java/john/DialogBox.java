4bbec43c4201dbfa64de941e9c4e66fb8080cc73
seanrh34 <34706751+seanrh34@users.noreply.github.com>
2025-09-04 21:19:12 +0800

    Change DialogBox GUI

--- src/main/java/john/DialogBox.java
+++ src/main/java/john/DialogBox.java
@@ -1,54 +1,62 @@
 package john;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+import java.util.Collections;
+
 /**
  * Simple dialog bubble for user and John messages.
  */
 public class DialogBox extends HBox {
+
+    private static final Image USER = new Image(DialogBox.class.getResourceAsStream("/images/DaUser.png"));
+    private static final Image JOHN = new Image(DialogBox.class.getResourceAsStream("/images/DaJohn.png"));
+
     @FXML
-    private Label text;
+    private Label dialog;
     @FXML
     private ImageView displayPicture;
 
     private DialogBox(String message, Image avatar, boolean isUser) {
-        super();
-        Label lbl = new Label(message);
-        lbl.setWrapText(true);
-        lbl.setMaxWidth(380);
-
-        ImageView iv = new ImageView(avatar);
-        iv.setFitWidth(40);
-        iv.setFitHeight(40);
-        iv.setPreserveRatio(true);
-
-        setSpacing(10);
-        setPadding(new Insets(8));
-        setAlignment(isUser ? Pos.TOP_RIGHT : Pos.TOP_LEFT);
-
-        if (isUser) {
-            getChildren().addAll(lbl, iv);
-            getStyleClass().add("user-bubble");
-        } else {
-            getChildren().addAll(iv, lbl);
-            getStyleClass().add("john-bubble");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        dialog.setText(message);
+        displayPicture.setImage(avatar);
     }
 
-    private static final Image USER = new Image(DialogBox.class.getResourceAsStream("/images/DaUser.png"));
-    private static final Image JOHN = new Image(DialogBox.class.getResourceAsStream("/images/DaJohn.png"));
+    private void flip() {
+        dialog.getStyleClass().add("reply-label");
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+    }
 
     public static DialogBox ofUser(String message) {
         return new DialogBox(message, USER, true);
     }
 
     public static DialogBox ofJohn(String message) {
-        return new DialogBox(message, JOHN, false);
+        DialogBox johnDb = new DialogBox(message, JOHN, false);
+        johnDb.flip();
+        return johnDb;
     }
 }
